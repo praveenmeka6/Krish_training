933f28c57fd65c9fda15ab333791a12039f18905
praveenmeka <praveenmeka6@gmail.com>
2022-02-28 23:44:29 +0530

    first commit

---
+++ assignment1/b/B.java
@@ -0,0 +1,5 @@
+public class B extends A{
+	public void run(){
+		System.out.println("B is running");
+	}
+}
---
+++ assignment1/c/Application.java
@@ -0,0 +1,6 @@
+public class Application{
+	public static void main(String[] args){
+		B b = new B();
+		b.run();
+	} 
+}
