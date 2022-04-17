c4f3fa12d17886dfcd37d5d6610531b10a7643b0
praveenmeka <praveenmeka6@gmail.com>
2022-04-18 01:02:06 +0530

    commit rentcloud commons model

---
+++ rentcloud-commons/src/main/java/com/praveen/rentcloud/commons/model/Customer.java
@@ -0,0 +1,54 @@
+package com.praveen.rentcloud.commons.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	
+	@Id
+	private int id;
+	
+	private String firstName;
+	private String lastName;
+	private String dlNumber;
+	private String zipCode;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDlNumber() {
+		return dlNumber;
+	}
+	public void setDlNumber(String dlNumber) {
+		this.dlNumber = dlNumber;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+}
